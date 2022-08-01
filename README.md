f41895f0706ab55fbc048e5c7ef7817888b15079
pkiop <pkiopb@gmail.com>
2022-08-02 00:45:09 +0900

    finish guessing game

--- README.md
+++ README.md
@@ -4,9 +4,12 @@
 * [Rust Crash Course](https://www.youtube.com/watch?v=zF34dRivLOw)
 * [Let's Code Snake with Rust and WASM](https://www.youtube.com/watch?v=iR7Q_6quwSI)
 
+### Doing
+
+* [Rust book](https://doc.rust-lang.org/book/) ~Ch2
+
 ### TODO
 
 * [Compiling from Rust to WebAssembly
 ](https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_wasm)
 * [yew](https://github.com/yewstack/yew) (Wasm framework for building client web apps)
-* [Rust book ](https://doc.rust-lang.org/book/)
--- book/guessing_game/src/main.rs
+++ book/guessing_game/src/main.rs
@@ -1,22 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
-// fn main() {
-//     println!("Guess the number!");
-//     println!("Please input your guess.");
-
-//     let mut guess = String::new();
-
-//     io::stdin().read_line(&mut guess).expect("Fail to read line");
-
-//     println!("You guessed: {}", guess);
-
-// }
-
 fn main() {
-    let mut a = [1, 2, 3, 4, 5];
-    let slice = &a[1..3];
-    let mut normal = a;
-    println!("{:?}", slice);
-    normal[2] = 2;
-    println!("{:?}", slice);
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Fail to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
