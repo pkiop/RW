4008f7f9d91a80eb406c93c9a66363b1db0eefe3
pkiop <pkiopb@gmail.com>
2022-08-03 23:34:39 +0900

    ch4 ownership

--- book/ch3_Common_Programing_Concepts/src/main.rs
+++ book/ch3_Common_Programing_Concepts/src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    let x = 5;
-    println!("The value of x is : {x}");
-    const XY: u8 = 6;
-    println!("The value of x is : {XY}");
-    another_function(55);
-}
-
-fn another_function(x: i32) {
-    println!("Another function. num is : {x}");
+    println!
 }
