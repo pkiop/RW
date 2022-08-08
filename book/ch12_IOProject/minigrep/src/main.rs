f245dd0c6ff6aaa0aa631fff0afe6a19d202d854
pkiop <pkiopb@gmail.com>
2022-08-08 20:44:25 +0900

    Feat: ~ch13

---
+++ book/ch12_IOProject/minigrep/src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    // output = ["target/debug/minigrep", "searchstring", "example-filename.txt"]
+    // output[0] = name of binary
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error : {}", e);
+        process::exit(1)
+    }
+}
+
+//separate logic
+// main.rs : main interface logic
+// lib.rs : program logic
