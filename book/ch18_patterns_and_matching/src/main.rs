93247ed756114bde22c0eacf0bbae2292085c667
pkiop <pkiopb@gmail.com>
2022-08-11 23:14:12 +0900

    ~ch18

--- book/ch18_patterns_and_matching/src/main.rs
+++ book/ch18_patterns_and_matching/src/main.rs
@@ -1,3 +1,56 @@
+// fn main() {
+//     let favorite_color: Option<&str> = None;
+//     let is_tuesday = false;
+//     let age: Result<u8, _> = "34".parse();
+
+//     if let Some(color) = favorite_color {
+//         println!("Using your favorite color, {}, as the background", color);
+//     } else if is_tuesday {
+//         println!("Tuesday is green day!")
+//     } else if let Ok(age) = age {
+//         if age > 30 {
+//             println!("Using purple as the background color");
+//         } else {
+//             println!("Using orange as the background color");
+//         }
+//     } else {
+//         println!("Using blue as the background color");
+//     }
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    let v = vec!['a', 'b', 'c'];
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    let point = (3, 5);
+    print_coordinates(&point);
+
+    // let Some(x) = 3; // couldn't cover
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+}
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
 }
