3ffdcb424e17580b45a689fa03c2896837aad7f6
pkiop <pkiopb@gmail.com>
2022-08-14 00:45:46 +0900

    rust test

---
+++ rust_in_action/hello/src/main.rs
@@ -0,0 +1,19 @@
+mod test;
+mod source;
+
+fn main() {
+    println!("Hello, world!");
+    println!("Hello test");
+    let t = String::from("Hello World");
+    println!("print string is {t}");
+
+    for i in 0..10 {
+        println!("{i}");
+    }
+
+    for j in 0..20 {
+        println!("{j} level");
+    }
+    test::test(); 
+    source::fn_in_source();
+}
---
+++ rust_in_action/hello/src/source.rs
@@ -0,0 +1,3 @@
+pub fn fn_in_source() {
+    println!("print");
+}
